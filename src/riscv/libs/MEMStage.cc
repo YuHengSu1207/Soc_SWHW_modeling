df45a9a8eb99c5b29c74828f1f037b097c3b9e22
YuhengSu <yuhengsu1207@gmail.com>
2025-04-18 00:45:18 +0800

    Feat: now sure why it's here

--- src/riscv/libs/MEMStage.cc
+++ src/riscv/libs/MEMStage.cc
@@ -9,7 +9,9 @@ void MEMStage::step() {
 		InstPacket* instPacket = (InstPacket*)this->getPipeRegister("prEXE2MEM-out")->value();
 
 		CLASS_INFO << " the MEM instruction @PC= " << instPacket->pc
-		           << "\n the WB instruction @PC= " << ((WBInstPacket) ? WBInstPacket->pc : 9487);
+		           << " instruction : " << instrToString(instPacket->inst.op)
+		           << "\n the WB instruction @PC= " << ((WBInstPacket) ? WBInstPacket->pc : 9487)
+		           << " instruction : " << ((WBInstPacket) ? instrToString(WBInstPacket->inst.op) : "nop");
 
 		if (is_MemPacket(instPacket)) {
 			is_mem_packet = true;
@@ -32,7 +34,9 @@ void MEMStage::step() {
 			if (!stall_ma) {
 				SimPacket* pkt = this->getPipeRegister("prEXE2MEM-out")->pop();
 				this->accept(currTick, *pkt);
-				if (is_mem_packet) { CLASS_INFO << "PC: " << ((InstPacket*)pkt)->pc << " Resolve the memory access"; }
+				// if(is_mem_packet){
+				//	CLASS_INFO << "PC: " << ((InstPacket*)pkt)->pc << " Resolve the memory access";
+				// }
 			} else {
 				this->forceStepInNextIteration();
 			}
@@ -41,8 +45,8 @@ void MEMStage::step() {
 }
 
 void MEMStage::instPacketHandler(acalsim::Tick when, SimPacket* pkt) {
-	CLASS_INFO << "   MEMStage::instPacketHandler(): Pushing InstPacket @PC=" << ((InstPacket*)pkt)->pc
-	           << " to prMEM2WB-in";
+	// CLASS_INFO << "   MEMStage::instPacketHandler(): Pushing InstPacket @PC=" << ((InstPacket*)pkt)->pc
+	//            << " to prMEM2WB-in";
 
 	if (!this->getPipeRegister("prMEM2WB-in")->push(pkt)) { CLASS_ERROR << "MEMStage failed to push InstPacket!"; }
 
